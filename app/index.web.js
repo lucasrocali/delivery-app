32eeb362b9c525cf804ca067932dcabdc0a444ff
Lucas Rocali <rocali@outlook.com>
2018-09-20 00:30:38 -0300

    Native + web

---
+++ app/index.web.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import App from './index.js';
+
+
+// register the app
+AppRegistry.registerComponent('App', () => App);
+
+AppRegistry.runApplication('App', {
+  initialProps: {},
+  rootTag: document.getElementById('react-app')
+});
